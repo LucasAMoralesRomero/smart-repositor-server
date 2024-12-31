be32e8939dbbc58ef6532b753f482d244f11d925
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-12-31 02:19:37 -0300

    Se agrega archivo de constantes del servidor

---
+++ config/constants.js
@@ -0,0 +1,4 @@
+// URL base para interactuar con la pagina web de Coto Digital
+const BASE_API_URL = "https://api.cotodigital.com.ar/sitios/cdigi/productos/_";
+
+module.exports = { BASE_API_URL };    
