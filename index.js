1b99f63142cefe77da0c7a8ca14d60e308d09786
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2025-01-01 17:50:03 -0300

    Se modifica index para hacerlo compatible con vercel

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Usamos el puerto de Vercel o 3000 por defecto
 
 // Middleware para manejar JSON (en caso de que se necesite)
 app.use(express.json());
 
 // Rutas principales
 app.use("/product", productRoutes);
+
+// Verificamos si estamos en producción
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction) {
+    console.log("Servidor ejecutándose en modo producción");
+} else {
+    console.log("Servidor ejecutándose en modo desarrollo");
+}
 
 // Iniciar el servidor
 app.listen(PORT, () => {
