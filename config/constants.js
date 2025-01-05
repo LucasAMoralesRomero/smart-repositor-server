8e72c739e0a7886bb1d47906bbce9f0cf0c78689
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2025-01-05 20:12:04 -0300

    Se define array de origenes permitidos

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,11 @@
 // URL base para interactuar con la pagina web de Coto Digital
 const BASE_API_URL = "https://api.cotodigital.com.ar/sitios/cdigi/productos/_";
-
-module.exports = { BASE_API_URL };    
+// Creamos el array con los dominios permitidos para configurar CORS
+const allowedOrigins = [
+    "https://smart-repositor.netlify.app",
+    "https://smart-repositor-test.netlify.app"
+];
+module.exports = { 
+    BASE_API_URL,
+    allowedOrigins
+ };    
